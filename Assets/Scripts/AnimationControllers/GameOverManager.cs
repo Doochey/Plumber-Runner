c45bdf7cdf8fa79d5d2159eb41d6caeafe3e8862
Doochey <doochey@live.co.uk>
2018-11-18 02:31:51 +0000

    Minor Changes

--- Assets/Scripts/AnimationControllers/GameOverManager.cs
+++ Assets/Scripts/AnimationControllers/GameOverManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,21 +7,27 @@ public class GameOverManager : MonoBehaviour
 {
 
 	public GameObject player;
-	public float time = 5f;
+	public GameObject gameOverScreen;
+	public HighscoreHandler highscoresHandler;
 
 	private Animator anim;
-
+	private bool gameOver;
+	
 	void Start()
 	{
+		gameOver = false;
 		anim = gameObject.GetComponent<Animator>();
 
 	}
 
 	void Update()
 	{
-		if (player == null)
+		if (player == null && !gameOver)
 		{
+			gameOver = true;
+			gameOverScreen.SetActive(true);
 			anim.SetTrigger("GameOver");
+			highscoresHandler.gameEnd ();
 			
 			// Enables Cursor
 			Cursor.visible = true;
