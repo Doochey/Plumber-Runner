1ed336a5cc40ec52b2c84aa34c9da0c074f2231e
Doochey <doochey@live.co.uk>
2018-12-09 16:37:58 +0000

    Asteroids split when hit with wrong projectile
    When an asteroid is hit with plasma it splits into 3 sub asteroids with 1 always moving towards the players position

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -42,7 +42,7 @@ public class EnemySpawner : MonoBehaviour {
 			Rigidbody enemyClone = Instantiate(spacehipPrefabs[Random.Range(0,spacehipPrefabs.Length)], 
 				(new Vector3(x, y, z)),transform.rotation);
 			speed = Random.Range(minSpeed, maxSpeed);
-			enemyClone.velocity = transform.up * speed;
+			enemyClone.velocity = transform.forward * speed;
 			eSpawned++;
 		}
 	}
