8c438fe3dd7420536bb0db1d02ae501a77da9408
qwertyman <ohendey@gmail.com>
2018-12-09 17:26:42 +0000

    Invincibility/Shields
    New invincibility/shield powerup. Also implemented grace period after taking damage. Yellow halo effect denotes both.

---
+++ Assets/DamageHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//used for invincibility, from powerups and taking damage
+public class DamageHandler : MonoBehaviour
+{
+	public GameObject player;
+	public int gracePeriod;
+
+	private bool invincible;
+
+	void Start ()
+	{
+		invincible = false;		
+	}
+
+	public bool isInvincible()
+	{
+		return invincible;
+	}
+
+	public void damageTaken()
+	{
+		StartCoroutine (Invincibility (gracePeriod));
+	}
+
+	public void TurnInvincible(int invincibilityTime)
+	{
+		StartCoroutine (Invincibility (invincibilityTime));
+	}
+
+	//coroutine to keep invincible true for a set amount of seconds
+	IEnumerator Invincibility(int invincibilityTime)
+	{
+		invincible = true;
+		player.GetComponent<Light>().enabled = true;
+		yield return new WaitForSeconds (invincibilityTime);
+		player.GetComponent<Light> ().enabled = false;
+		invincible = false;
+	}
+}
--- Assets/Scripts/ObstacleCollision.cs
+++ Assets/Scripts/ObstacleCollision.cs
@@ -7,39 +7,51 @@ public class ObstacleCollision : MonoBehaviour
 {
 	public int damageDealt;
     public GameObject collisionSound;
+	public GameObject gameController;
+
+	private void Start()
+	{
+		gameController = GameObject.FindWithTag ("GameController");
+	}
 
     void OnTriggerEnter(Collider other)
 	{
         if ( other.gameObject.CompareTag("Player"))
 		{
+			GameObject player = other.gameObject;
+
             // Play the explosion sound
             GameObject makeSound = Instantiate(collisionSound, this.transform.position, this.transform.rotation) as GameObject;
 
             //play particle explosion
-            var exp = other.gameObject.GetComponent<ParticleSystem>();
+            var exp = player.GetComponent<ParticleSystem>();
 			exp.Play();
 
-			//inflict damage to player
-			other.gameObject.GetComponent<ShipHealth> ().takeDamage (damageDealt);
+			//inflict damage to player if they're not invincible, and start grace period
+			if (!gameController.GetComponent<DamageHandler>().isInvincible ())
+			{
+				player.GetComponent<ShipHealth>().takeDamage (damageDealt);
+				gameController.GetComponent<DamageHandler> ().damageTaken();
+			}
 
 			//if player has no health left, destroy it
-			if (other.gameObject.GetComponent<ShipHealth>().getHealth () <= 0)
+			if (player.GetComponent<ShipHealth>().getHealth () <= 0)
 			{
 				// Disables player movement
-				other.gameObject.GetComponent<Movement>().enabled = false;
+				player.GetComponent<Movement>().enabled = false;
 				
 			
 				// Disables mesh renderer of player object making them invisible
-				Renderer rend = other.gameObject.GetComponent<Renderer>();
+				Renderer rend = player.GetComponent<Renderer>();
 				rend.enabled = false;
 			
 				// Disables all children of the player object
-				foreach (Transform child in other.gameObject.transform)
+				foreach (Transform child in player.transform)
 				{
 					child.gameObject.SetActive(false);
 				}
 				
-				Destroy(other.gameObject, exp.main.duration);
+				Destroy(player, exp.main.duration);
 			}
 			//otherwise destroy obstacle
 			else
