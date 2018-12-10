8a19cfc1e159ae08c6da9032aacac061092e59b1
qwertyman <ohendey@gmail.com>
2018-12-10 19:20:45 +0000

    Firerate Powerup Started

---
+++ Assets/FireRateHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRateHandler : PowerupHandler
+{
+	public int fireRateMultiplier;
+
+	void Start ()
+	{
+		
+	}
+
+	public override void Apply()
+	{
+		
+	}
+}
---
+++ Assets/Powerups/Scripts/HealthUpHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUpHandler : PowerupHandler
+{
+	public int healAmount;
+
+	private DamageHandler damageHandler;
+
+	void Start ()
+	{
+		GameObject gameController = GameObject.FindWithTag("GameController");
+		damageHandler = gameController.GetComponent<DamageHandler> ();
+	}
+
+	public override void Apply()
+	{
+		damageHandler.GetComponent<DamageHandler> ().heal (healAmount);
+	}
+}
